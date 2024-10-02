326225bbaf5260527fd3190389ebefc295eef880
treenidad <ktrinidad209@gmail.com>
2024-10-02 00:20:10 -0700

    Updates

--- index.js
+++ index.js
@@ -18,44 +18,21 @@ function calculate() {
   }
 }
 
-// Testing for capturing the keypresses
-documet.addEventListener("keydown", (event) => {
-  console.log(event);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    // Call your function here
+    clearDisplay();
+  }
 });
 
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    // Call your function here
+    calculate();
+  }
+});
+
+// Testing for capturing the keypresses
 // document.addEventListener("keydown", (event) => {
-//   if (event.key == "Equal" || "Enter") {
-//   }
-//   keyboardInput.value += event.key;
-// });
-
-// $(document).ready(function () {
-//   var string = "";
-
-//   /* Calculator input string */
-//   $(".value").click(function () {
-//     string += $(this).text();
-//     $("#calc").text(string);
-//   });
-
-//   /* Clear all */
-//   $(".C").click(function () {
-//     string = "";
-//     $("#calc, #result").text("0");
-//   });
-//   /* Clear last entry */
-//   $(".CE").click(function () {
-//     string = string.slice(0, string.length - 1);
-//     $("#calc").text(string);
-//   });
-
-//   /* Show result */
-//   $(".equals").click(function () {
-//     $("#result").text(eval(string));
-//   });
-// });
-//   /* Show result */
-//   $(".equals").click(function () {
-//     $("#result").text(eval(string));
-//   });
+//   console.log(event);
 // });
